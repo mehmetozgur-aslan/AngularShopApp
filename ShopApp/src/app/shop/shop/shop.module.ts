5f3c2657b50542d83a089ccb261724ae6d15ab1c
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-07 09:00:50 +0300

    navbar olusturuldu

--- ShopApp/src/app/shop/shop/shop.module.ts
+++ ShopApp/src/app/shop/shop/shop.module.ts
@@ -3,12 +3,13 @@ import { ModelModule } from '../../model/model.module';
 import { ShopComponent } from './shop.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   imports:[ModelModule,BrowserModule,FormsModule],//Model klasörü içindeki herşeye erişim
   providers:[],
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, NavbarComponent],
   exports:[ShopComponent] // ShopComponent dışarı açılır.
   })
 
