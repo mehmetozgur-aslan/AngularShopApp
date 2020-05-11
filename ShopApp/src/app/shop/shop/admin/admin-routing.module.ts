61b396e590625e74a5eaadfead82a58ca7d110eb
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-11 09:21:10 +0300

    admin sayfalari olusturuldu

--- ShopApp/src/app/shop/shop/admin/admin-routing.module.ts
+++ ShopApp/src/app/shop/shop/admin/admin-routing.module.ts
@@ -3,11 +3,29 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuard } from './auth.guard';
+import { ProductFormComponent } from './admin/products/product-form/product-form.component';
+import { ProductListComponent } from './admin/products/product-list/product-list.component';
+import { CategoryFormComponent } from './admin/categories/category-form/category-form.component';
+import { CategoryListComponent } from './admin/categories/category-list/category-list.component';
+import { OrderListComponent } from './admin/orders/order-list/order-list.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'main', component: AdminComponent,canActivate:[AuthGuard] },
-  { path: '**', redirectTo: 'auth'},
+  {
+    path: 'main',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'products/:mode/:id', component: ProductFormComponent },
+      { path: 'products/:mode/', component: ProductFormComponent },
+      { path: 'products', component: ProductListComponent },
+      { path: 'categories/:mode/:id', component: CategoryFormComponent },
+      { path: 'categories/:mode/', component: CategoryFormComponent },
+      { path: 'categories', component: CategoryListComponent },
+      { path: 'orders', component: OrderListComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
--- ShopApp/src/app/shop/shop/admin/admin.module.ts
+++ ShopApp/src/app/shop/shop/admin/admin.module.ts
@@ -5,10 +5,15 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth.guard';
+import { ProductListComponent } from './admin/products/product-list/product-list.component';
+import { ProductFormComponent } from './admin/products/product-form/product-form.component';
+import { CategoryListComponent } from './admin/categories/category-list/category-list.component';
+import { CategoryFormComponent } from './admin/categories/category-form/category-form.component';
+import { OrderListComponent } from './admin/orders/order-list/order-list.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, AuthComponent],
+  declarations: [AdminComponent, AuthComponent, ProductListComponent, ProductFormComponent, CategoryListComponent, CategoryFormComponent, OrderListComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
