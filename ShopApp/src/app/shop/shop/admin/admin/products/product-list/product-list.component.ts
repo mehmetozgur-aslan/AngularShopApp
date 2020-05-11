524625fe6ae575fbced6baed3a9949caec7589c4
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-11 09:26:33 +0300

    admin product list

--- ShopApp/src/app/shop/shop/admin/admin/products/product-list/product-list.component.ts
+++ ShopApp/src/app/shop/shop/admin/admin/products/product-list/product-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductRepository } from 'src/app/model/product.repository';
+import { Product } from 'src/app/model/product.model';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  constructor(private productRepository: ProductRepository) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getProducts(): Product[] {
+    return this.productRepository.getProducts();
   }
 
+  deleteProduct(p: Product) {}
 }
